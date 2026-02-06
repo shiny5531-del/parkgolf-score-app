7b2409df28deb5a8b44a7a2876b4ca94839f44a8
shiny5531-del <shiny5531@gmail.com>
2026-02-06 22:13:11 +0900

    Create HomeScreen component

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeScreen = () => {
+    return (
+        <div>
+            <h1>Welcome to the Park Golf Score App</h1>
+        </div>
+    );
+};
+
+export default HomeScreen;
